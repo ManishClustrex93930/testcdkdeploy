6adeee5bfb0b0d35cd32200c8e089266f91f41a4
ManishClustrex93930 <manish.clustrex@gmail.com>
2025-05-24 12:08:45 +0530

    up

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-# Get the GitHub PAT from environment variable
-GITHUB_PAT="$GITHUB_PAT"
+BRANCH_NAME="main"
 
-# Validate that GITHUB_PAT is set
-if [ -z "$GITHUB_PAT" ]; then
-  echo "Error: GITHUB_PAT environment variable not set."
-  exit 1
-fi
+# Ensure you're on the correct branch
+git checkout "${BRANCH_NAME}"
+git pull origin "${BRANCH_NAME}"
 
-REPO_URL="https://${GITHUB_PAT}@github.com/ManishClustrex93930/testcdkdeploy.git"
-BRANCH="main"
-
-# Remove old clone if exists
-rm -rf cdk-app
-
-# Clone the repo and switch to it
-git clone --branch "$BRANCH" "$REPO_URL" cdk-app
-cd cdk-app
-
+# Install Python dependencies
 pip install -r requirements.txt
 
-# Bootstrap the environment
-cdk bootstrap aws://851725206270/us-east-2 --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess --force --toolkit-stack-name CDKToolkit
+# Bootstrap CDK
+cdk bootstrap aws://851725206270/us-east-2 \
+  --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess \
+  --force \
+  --toolkit-stack-name CDKToolkit
 
-
-# Synthesize CloudFormation templates
+# Synthesize and deploy
 cdk synth
-
-# Deploy the stack without approval prompts
 cdk deploy --require-approval never
